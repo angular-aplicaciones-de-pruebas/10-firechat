4a8ef3b28eb17299ecaa893f62a6e235471ad343
Hernan Arena <hernan.arena@gmail.com>
2019-02-15 02:46:31 -0300

    first commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBlZqQOPwjkAC2iDOdT9bu0Q7FMyVHoqlY",
+      authDomain: "firechat-a31df.firebaseapp.com",
+      databaseURL: "https://firechat-a31df.firebaseio.com",
+      projectId: "firechat-a31df",
+      storageBucket: "firechat-a31df.appspot.com",
+      messagingSenderId: "30885208021"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
